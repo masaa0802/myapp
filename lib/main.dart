3c454891ace384585a5298d7b991021402dc703b
masaa0802 <masaakigoto0802@gmail.com>
2022-08-02 00:12:48 +0900

    second

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+            theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+
+           
+
+            // 太さを指定
+            const Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
+
+            // スタイルを指定
+            const Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
+            // サイズを指定
+            const Text('fontSize = 36', style: TextStyle(fontSize: 36)),
+            // 色を指定
+            const Text('Red', style: TextStyle(color: Colors.red)),
+            Container(
+              width: double.infinity,
+              color: Colors.grey,
+              // 表示位置を指定
+              child: const Text('TextAlign.right', textAlign: TextAlign.right),
+            ),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
