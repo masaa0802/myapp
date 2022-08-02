661f00fda8c0841669676823b2028d20fea0572b
masaa0802 <masaakigoto0802@gmail.com>
2022-08-03 00:25:14 +0900

    サイズ・背景・余白・枠線

--- lib/main.dart
+++ lib/main.dart
@@ -1,128 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-            theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-
-           
-
-            // 太さを指定
-            const Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
-
-            // スタイルを指定
-            const Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
-            // サイズを指定
-            const Text('fontSize = 36', style: TextStyle(fontSize: 36)),
-            // 色を指定
-            const Text('Red', style: TextStyle(color: Colors.red)),
-            Container(
-              width: double.infinity,
-              color: Colors.grey,
-              // 表示位置を指定
-              child: const Text('TextAlign.right', textAlign: TextAlign.right),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                // 背景色
+                color: Colors.blue,
+                child: Text('blue'),
+              ),
+              Container(
+                // 横幅
+                width: 200,
+                // 縦幅
+                height: 50,
+                color: Colors.blue,
+                child: Text('200x50'),
+              ),
+              Container(
+                // 内側の余白
+                padding: EdgeInsets.all(8),
+                color: Colors.blue,
+                child: Text('padding'),
+              ),
+              Container(
+                color: Colors.blue,
+                child: Container(
+                  // 外側の余白
+                  margin: EdgeInsets.all(8),
+                  color: Colors.green,
+                  child: Text('margin'),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  // 枠線
+                  border: Border.all(color: Colors.blue, width: 2),
+                  // 角丸
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.all(8),
+                child: Text('border'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  // 背景画像
+                  image: DecorationImage(
+                    image: NetworkImage('https://placehold.jp/200x100.png'),
+                  ),
+                ),
+                width: 200,
+                height: 100,
+                child: Text('image'),
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
