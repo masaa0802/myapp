ca3905e70f93dc1eaca061c3c823ddc544784726
masaa0802 <masaakigoto0802@gmail.com>
2022-09-04 14:58:45 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
